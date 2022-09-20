e5eb07521acb60b8c815257c3a02257f1d577b10
Bharath <91299756+BharathKatabathuni@users.noreply.github.com>
2022-09-19 23:53:43 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SE Homework1',
+    version='1.0.0',
+    packages=find_packages(),
+    description='SE repository',
+    tests_require=['test'],
+    author='Katabathuni, Bharath',
+    author_email='bharathkatabathuni@gmail.com',
+    url='https://github.com/vamsitadikonda/csv-summarizer',
+    python_requires='>=3.7',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Topic :: Homework1",
+    ],
+    license='MIT'
+)
