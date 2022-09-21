185df5e77acdd6a9e778534bf0229f1776840598
Swimitha <swimitha@gmail.com>
2022-09-21 02:27:37 -0400

    Adding remaining testcases

--- src/types/Data.py
+++ src/types/Data.py
@@ -1,16 +1,16 @@
 from src.types.Obj import Obj
 from src.types.Cols import Cols
 from src.types.Row import Row
-from src.utils import csv, push, rnd
+from src.utils import csv, rnd
 
 
 class Data(Obj):
     def __init__(self, src):
         super().__init__("Data")
         self.cols = None
-        self.rows = {}
+        self.rows = []
         if isinstance(src, str):
-            csv(src, lambda x: self.add(x))
+            csv(src, self.add)
         elif isinstance(src, dict):
             for row in src.values():
                 self.add(row)
@@ -20,17 +20,17 @@ class Data(Obj):
         if not self.cols:
             self.cols = Cols(xs)
         else:
-            row = push(self.rows, xs if xs.cells else Row(xs))
-            for _, todo in enumerate(self.cols.x, self.cols.y):
-                for _, col in enumerate(todo):
-                    col.add(row.cells[col.at])
-        return
+            row = xs if hasattr(xs, 'cell') and xs.cells else Row(xs)
+            self.rows.append(row)
+            for _, col in enumerate(self.cols.x + self.cols.y):
+                col.add(row.cells[col.at])
 
     def stats(self, places, showCols=None, fun="mid"):
         if showCols is None:
             showCols = self.cols
+        print(showCols)
         t = {}
-        for col in showCols.values():
+        for col in showCols:
             v = fun(col)
             v = v.isnumeric() and rnd(v, places) or v
             t[col.name] = v
